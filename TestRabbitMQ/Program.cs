1d1a6b563d9d5aa0e7c6b2ebba9708e6779db15a
development <development@tntsoft.onmicrosoft.com>
2020-05-26 09:47:44 +0200

    Add project files.

---
+++ TestRabbitMQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
