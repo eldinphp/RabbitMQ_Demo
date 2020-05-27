a90d0222d8ad836ea378f874b2a4b0b72abffba2
development <development@tntsoft.onmicrosoft.com>
2020-05-27 16:27:05 +0200

    Works

---
+++ TestRabbitMQ/MyBasicConsumer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TestRabbitMQ
+{
+    class MyBasicConsumer : IBasicConsumer
+    {
+        public IModel Model => throw new NotImplementedException();
+
+        public event EventHandler<ConsumerEventArgs> ConsumerCancelled;
+
+        public void HandleBasicCancel(string consumerTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleBasicCancelOk(string consumerTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleBasicConsumeOk(string consumerTag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, ReadOnlyMemory<byte> body)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleModelShutdown(object model, ShutdownEventArgs reason)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestRabbitMQ/Program.cs
+++ TestRabbitMQ/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestRabbitMQ
 {
     class Program
     {
+        public static string QueueName = "My6Queue";
+        public static string ExchangeName = "My6Exchange";
+
         static void Main(string[] args)
         {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            var connection = factory.CreateConnection();
+            var model = connection.CreateModel();
+            model.ExchangeDeclare(ExchangeName, "direct", true, true, null);
+            model.QueueDeclare(QueueName, true, false, true, null);
+            model.QueueBind(QueueName, ExchangeName, "");
             
+            
+            var consumer = new EventingBasicConsumer(model);
+            
+            consumer.Received += (m, ev) =>
+            {
+                var readMessage = ev.Body;
+            };
+
+            var numberOfMessages = model.MessageCount(QueueName);
+            var message = Encoding.ASCII.GetBytes("Ovo je druga testna poruka");
+            model.BasicPublish("", QueueName, null, message); //CANT PUBLISH TO EXCHANGE NAME ?
+            numberOfMessages = model.MessageCount(QueueName);
+            numberOfMessages = model.MessageCount(QueueName);
+            Thread.Sleep(200);
+            model.BasicConsume(QueueName, autoAck: false, consumer: consumer);
+            numberOfMessages = model.MessageCount(QueueName);
+            numberOfMessages = model.MessageCount(QueueName);
+            Thread.Sleep(200);
+            numberOfMessages = model.MessageCount(QueueName);
+            numberOfMessages = model.MessageCount(QueueName);
         }
     }
 }
